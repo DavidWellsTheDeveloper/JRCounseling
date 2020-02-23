02134a9d39fbd26fedca23d3b160ed58aab00275
DavidWellsTheDeveloper <dave1.t.wells@gmail.com>
2020-02-23 13:32:09 -0700

    try to fix favicon

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <!-- Bootstrap CSS -->
-  <link rel="shortcut icon" href="{{ asset('img/favicon.ico') }}">
+  <link rel="icon" type="img/png" sizes="32x32" href="{{ favicon(asset('favicon-32x32.png')) }}">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
   <link rel="stylesheet" href="css/app.css">
   <script defer src="js/font-awesome-all.js"></script>
